48fd299a9fb567cbcc61a780329ad134ca5d49ed
vairunejaimaka <thanakon63110@gmail.com>
2026-04-11 14:22:52 +0700

    prepare for deploy

--- app.py
+++ app.py
@@ -484,4 +484,5 @@ def log_time(start_time):
     print(f"⏱️ RESPONSE TIME: {duration:.2f} seconds")
 
 if __name__ == "__main__":
-    app.run(port=8000, debug=True) 
+    port = int(os.environ.get('PORT', 8000))
+    app.run(host="0.0.0.0", port=port) 
